554376d4f494144d71ed90a83cfedd403e1b2987
TanHaodong <1141637214@qq.com>
2022-07-06 17:57:49 +0800

    v1.03

---
+++ lib/pages/welcome/welcome.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:news_app/common/utils/utils.dart';
+import 'package:news_app/common/values/values.dart';
+
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  // 页头标题
+  Widget _buildPageHeadTitle() {
+    return Container(
+      margin: EdgeInsets.only(top: duSetHeight(60)),
+      child: Text(
+        "Features",
+        style: TextStyle(
+          color: AppColors.primaryText,
+          fontFamily: "Montserrat",
+          fontWeight: FontWeight.w600,
+          fontSize: duSetFontSize(30),
+        ),
+      ),
+    );
+  }
+
+  // 页头说明
+  Widget _buildPageHeaderDetail() {
+    return Container(
+      width: duSetWidth(242),
+      height: duSetHeight(70),
+      margin: EdgeInsets.only(top: duSetHeight(14)),
+      child:  Text(
+        "The best of news channels all in one place. Trusted sources and personalized new for you.",
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: AppColors.primaryText,
+          fontFamily: "Avenir",
+          fontWeight: FontWeight.normal,
+          fontSize: duSetFontSize(16),
+          height: 1.3,  // 字间距
+        ),
+      ),
+    );
+  }
+
+  // 特性说明
+  Widget _buildFeatureItem(String imageName,String intro,double marginTop) {
+    return Container(
+      width: duSetWidth(295),
+      height: duSetHeight(80),
+      margin: EdgeInsets.only(top: duSetHeight(marginTop)),
+      child: Row(
+        children: [
+          Container(
+            width: duSetWidth(80),
+            height: duSetHeight(80),
+            child: Image.asset(
+              "assets/images/$imageName.png",
+              fit: BoxFit.none,
+            ),
+          ),
+          const Spacer(),
+          Container(
+            width: duSetWidth(195),
+            child: Text(
+              intro,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: AppColors.primaryText,
+                fontFamily: "Avenir",
+                fontWeight: FontWeight.normal,
+                fontSize: duSetFontSize(16),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  // 开始按钮
+  Widget _buildStartButton() {
+    return Container(
+      width: duSetWidth(295),
+      height: duSetHeight(44),
+      margin: EdgeInsets.only(bottom: duSetHeight(20)),
+      child: FlatButton(
+        color: AppColors.primaryElement,
+        textColor: AppColors.primaryElementText,
+        child: Text('Get Started'),
+        shape: const RoundedRectangleBorder(
+          // borderRadius: BorderRadius.all(Radius.circular(duSetWidth(6))),
+            borderRadius:Radii.k6pxRadius,
+        ),
+        onPressed: (){},
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(context,
+        designSize: const Size(375, 812 - 44 - 34),
+        minTextAdapt: true,
+        splitScreenMode: true);
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            _buildPageHeadTitle(),
+            _buildPageHeaderDetail(),
+            _buildFeatureItem(
+              "feature-1",
+              "Compelling photography and typography provide a beautiful reading",
+              86,
+            ),
+            _buildFeatureItem(
+              "feature-2",
+              "Sector news never shares your personal data with advertisers or publishers",
+              40,
+            ),
+            _buildFeatureItem(
+              "feature-3",
+              "You can get Premium to unlock hundreds of publications",
+              40,
+            ),
+            Spacer(),
+            _buildStartButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
